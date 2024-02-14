7677cc80a507dc809db3eef74a52593087532349
Melnar Ancit <39809315+narancit@users.noreply.github.com>
2024-02-14 17:23:47 +0800

    Update recursive-list.js

--- recursive-list.js
+++ recursive-list.js
@@ -23,11 +23,15 @@ function arrayToList(array) {
 }
 
 function listToArray(obj) {
-  array.push(obj.value)
-  if(obj.rest != null) {
-    listToArray(obj.rest) 
+  let array = []
+  function recursion(obj) {
+    array.push(obj.value)
+    if(obj.rest != null) {
+      recursion(obj.rest) 
+   	}
+    return array
   }
-  return array
+  return recursion(obj)
 }
 
 function prepend(value, obj) {
@@ -38,13 +42,12 @@ function nth(obj, index) {
   return listToArray(obj)[index]
 }
 
-console.log(arrayToList([10, 20, 30]));
+console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-let array = []
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-array = [] // clear 
 console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
 // → 20
